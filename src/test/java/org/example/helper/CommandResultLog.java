21a8db038e383807ec925f70c8e3a421ce578ce8
FurkanKartalTSTNM <furkan.kartal@testinium.com>
2025-10-07 17:53:34 +0300

    New Deneme

--- src/test/java/org/example/helper/CommandResultLog.java
+++ src/test/java/org/example/helper/CommandResultLog.java
@@ -1,20 +1,40 @@
 package org.example.helper;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class CommandResultLog {
     private String screenshotName;
-    private String method;       // HTTP method bilmiyorsan command name'i yaz
+    private String method;
     private String requestData;
     private String responseData;
-    private String requestPath;  // command name
-    private long startDate;
-    private int runtime;         // ms
-    private long endDate;
-    private String level;        // PASS/FAIL/ERROR/INFO
+    private String requestPath;
+    private long   startDate;
+    private int    runtime;
+    private long   endDate;
+    private String level;
 
-    // getters & setters ...
+    public String getScreenshotName() { return screenshotName; }
+    public void setScreenshotName(String screenshotName) { this.screenshotName = screenshotName; }
+
+    public String getMethod() { return method; }
+    public void setMethod(String method) { this.method = method; }
+
+    public String getRequestData() { return requestData; }
+    public void setRequestData(String requestData) { this.requestData = requestData; }
+
+    public String getResponseData() { return responseData; }
+    public void setResponseData(String responseData) { this.responseData = responseData; }
+
+    public String getRequestPath() { return requestPath; }
+    public void setRequestPath(String requestPath) { this.requestPath = requestPath; }
+
+    public long getStartDate() { return startDate; }
+    public void setStartDate(long startDate) { this.startDate = startDate; }
+
+    public int getRuntime() { return runtime; }
+    public void setRuntime(int runtime) { this.runtime = runtime; }
+
+    public long getEndDate() { return endDate; }
+    public void setEndDate(long endDate) { this.endDate = endDate; }
+
+    public String getLevel() { return level; }
+    public void setLevel(String level) { this.level = level; }
 }
